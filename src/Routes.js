74ce73807b31e5f4bb4bb0ea209532af27ef46f7
Ratchitta <59676467+MKMKsGit@users.noreply.github.com>
2023-02-08 14:56:56 +0700

    JAM-145 routing at landing page

--- src/Routes.js
+++ src/Routes.js
@@ -1,5 +1,16 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
 const MyRoutes = () => {
-    return <></>;
+    return (<BrowserRouter>
+            <Routes>
+                <Route path="/">
+                    <Route index element={<Landing />}/>
+                    <Route path="forgetpassword" element={<Password />}/>
+                    <Route path=":id" element={<Layout />}/>
+                    <Route path="*" elemment={<NotFound />}/>
+                </Route>
+            </Routes>
+        </BrowserRouter>);
 }
 
 export default MyRoutes;
